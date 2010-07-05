9b63e4ab459c84a779a6f341f215bdcc434b5e4e
Michi Kono <michikono@gmail.com>
2010-07-05 10:13:41 -0700

    Removed debug output and fixed formatting

--- src/ezajaxer.js
+++ src/ezajaxer.js
@@ -26,7 +26,6 @@
             var ezajaxer = new Ezajaxer($.metadata ? $.extend({}, settings, $this.metadata()) : settings);
             ezajaxer.attach($this);
             
-//            console.log($(this).data());
             return $(this);
         });
     };    
@@ -70,47 +69,47 @@ var Ezajaxer = (function(userConfig) {
      */
     this.dom = (function(config) {
         return {
-        /**
-         * Mark a DOM object as altered by this class so additional
-         * event handlers are added to it
-         * 
-         * @param DOM element
-         * @return jQuery DOM element
-         */
-        mark: function(element) {
-            return $(element).data(config.marker, config.marker);
-        },
-        
-        /**
-         * Unmark a DOM object that was altered by mark()
-         * 
-         * @param DOM element
-         * @return jQuery DOM element
-         */
-        unmark: function(element) {
-            return $(element).data(config.marker, null);
-        },
-        
-        /**
-         * Tests if an element has been marked
-         *
-         * @param DOM element
-         * @return Returns the marker name associated to this element
-         */
-        getMarker: function(element) {
-            return $(element).data(config.marker);
-        },
-        
-        /**
-         * Tests if an element has been marked
-         *
-         * @param DOM element
-         * @return TRUE if has been marked
-         */
-        hasMarker: function(element) {
-            return Boolean($(element).data(config.marker) || false);
-        }
-    };
+            /**
+             * Mark a DOM object as altered by this class so additional
+             * event handlers are added to it
+             * 
+             * @param DOM element
+             * @return jQuery DOM element
+             */
+            mark: function(element) {
+                return $(element).data(config.marker, config.marker);
+            },
+            
+            /**
+             * Unmark a DOM object that was altered by mark()
+             * 
+             * @param DOM element
+             * @return jQuery DOM element
+             */
+            unmark: function(element) {
+                return $(element).data(config.marker, null);
+            },
+            
+            /**
+             * Tests if an element has been marked
+             *
+             * @param DOM element
+             * @return Returns the marker name associated to this element
+             */
+            getMarker: function(element) {
+                return $(element).data(config.marker);
+            },
+            
+            /**
+             * Tests if an element has been marked
+             *
+             * @param DOM element
+             * @return TRUE if has been marked
+             */
+            hasMarker: function(element) {
+                return Boolean($(element).data(config.marker) || false);
+            }
+        };
     }(this.config));
 });
 
